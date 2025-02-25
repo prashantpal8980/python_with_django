7f3bec013a37b70968e7684eb844ad27f93ff901
prashantpal8980 <palprashant8980@gmail.com>
2025-02-25 23:15:37 +0530

    i edited views.py by importing from shortcuts improt render and

--- personalWebsite/personalWebsite/views.py
+++ personalWebsite/personalWebsite/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, Prashant here')
+   # return HttpResponse('Hello, Prashant here')
+   return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('Well i am a Cyber Security Enthusiast and love linux over windows')
