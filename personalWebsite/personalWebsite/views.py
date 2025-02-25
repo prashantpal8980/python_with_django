623c532e23d9c0398b1e1357d4ee734274524ef8
prashantpal8980 <palprashant8980@gmail.com>
2025-02-26 00:52:09 +0530

    I have created about contact and hobbies page and added css for each page now i want to change index.html so that i can navigate to all these pages with index page

--- personalWebsite/personalWebsite/views.py
+++ personalWebsite/personalWebsite/views.py
@@ -6,10 +6,13 @@ def home(request):
    return render(request, 'website/index.html')
 
 def about(request):
-    return HttpResponse('Well i am a Cyber Security Enthusiast and love linux over windows')
+    # return HttpResponse('Well i am a Cyber Security Enthusiast and love linux over windows')
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse('You can contact me at my email or at this website')
+    # return HttpResponse('You can contact me at my email or at this website')
+    return render(request, 'website/contact.html')
 
 def hobbies(request):
-    return HttpResponse('I love to liten to music ')
+    # return HttpResponse('I love to liten to music ')
+    return render(request, 'website/hobbies.html')
