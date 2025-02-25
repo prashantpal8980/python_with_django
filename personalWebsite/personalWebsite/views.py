c0176a09fafa1acdd31bdf1e5fc239ac29c95da3
prashantpal8980 <palprashant8980@gmail.com>
2025-02-25 23:10:08 +0530

    basic reponse through views.py and urls.py

---
+++ personalWebsite/personalWebsite/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello, Prashant here')
+
+def about(request):
+    return HttpResponse('Well i am a Cyber Security Enthusiast and love linux over windows')
+
+def contact(request):
+    return HttpResponse('You can contact me at my email or at this website')
+
+def hobbies(request):
+    return HttpResponse('I love to liten to music ')
