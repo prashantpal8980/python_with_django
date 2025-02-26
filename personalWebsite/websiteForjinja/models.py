284fbd0e57e71e2ddc0ac207e46a27cc7b9fc7dd
prashantpal8980 <palprashant8980@gmail.com>
2025-02-26 12:56:57 +0530

    first model is created

--- personalWebsite/websiteForjinja/models.py
+++ personalWebsite/websiteForjinja/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class TellTheStroy(models.Model):
+    GROUP_MEMBERS=[
+        ('PP', 'Prashant Pal'),
+        ('AP', 'Aashsih Patel'),
+        ('BP', 'Bittu Kumar'),
+        ('DJ', 'Deepak Jha'),
+    ]
+    name = models.CharField(max_length=2, choices=GROUP_MEMBERS)
+    image = models.ImageField(upload_to='images/')
+    date_posted = models.DateTimeField(default=timezone.now)
+
