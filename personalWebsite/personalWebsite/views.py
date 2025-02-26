2c726e53fcf4966980ed110eff2d6f157c46b430
prashantpal8980 <palprashant8980@gmail.com>
2025-02-27 00:53:00 +0530

    PageToSeeIncident is the best here

--- personalWebsite/personalWebsite/views.py
+++ personalWebsite/personalWebsite/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 def home(request):
    # return HttpResponse('Hello, Prashant here')
@@ -16,3 +17,4 @@ def contact(request):
 def hobbies(request):
     # return HttpResponse('I love to liten to music ')
     return render(request, 'website/hobbies.html')
+
--- personalWebsite/websiteForjinja/views.py
+++ personalWebsite/websiteForjinja/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import TellTheStroy
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def Education(request):
     # StoryData=TellTheStroy.objects.all()
@@ -9,3 +10,7 @@ def Education(request):
 def incidentReturn(request):
     StoryData=TellTheStroy.objects.all()
     return render(request, 'PageToSeeincident.html',{'Alldata':StoryData})
+
+def incidentDetail(request,incident_id):
+    aboutIncident=get_object_or_404(TellTheStroy,pk=incident_id)
+    return render(request, 'incidentDetail.html',{'incident':aboutIncident})
