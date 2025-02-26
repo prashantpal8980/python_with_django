3d20a3417d0aa18c109133643d135053f0307505
prashantpal8980 <palprashant8980@gmail.com>
2025-02-26 16:26:55 +0530

    i have edited models.py file      def __str__(self):         return self.name by this on admin panel it showing the name who saved it

--- personalWebsite/websiteForjinja/models.py
+++ personalWebsite/websiteForjinja/models.py
@@ -3,14 +3,22 @@ from django.utils import timezone
 
 # Create your models here.
 class TellTheStroy(models.Model):
+    # GROUP_MEMBERS=[
+    #     ('PP', 'Prashant Pal'),
+    #     ('AP', 'Aashsih Patel'),
+    #     ('BP', 'Bittu Kumar'),
+    #     ('DJ', 'Deepak Jha'),
+    # ]
     GROUP_MEMBERS=[
-        ('PP', 'Prashant Pal'),
-        ('AP', 'Aashsih Patel'),
-        ('BP', 'Bittu Kumar'),
-        ('DJ', 'Deepak Jha'),
+        ('Prashant Pal', 'Prashant Pal'),
+        ('Ashish Patel', 'Ashsih Patel'),
+        ('Bittu Kumar', 'Bittu Kumar'),
+        ('Deepak Jha', 'Deepak Jha'),
     ]
-    name = models.CharField(max_length=2, choices=GROUP_MEMBERS)
+    name = models.CharField(max_length=20, choices=GROUP_MEMBERS)
     story= models.TextField(max_length=1000,default='Once upon a time...')
     image = models.ImageField(upload_to='images/')
     date_posted = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.name
